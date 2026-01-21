b72c762389508732a1976fdaa2c2580a2e1b8d17
Amir <93218051+arzjaking@users.noreply.github.com>
2026-01-21 11:15:47 +0500

    Add files via upload

---
+++ dryfruits/js/main_data.js
@@ -0,0 +1,213 @@
+export const itemsData = [
+    {
+        image: './Assets/images/izyum-sayaki.jpg',
+        title: 'Черный Изюм',
+        subtitle: [
+            'Сорт - высший',
+            'Тип сушки - Теневая',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/maiz-golden.jpg',
+        title: 'Изюм Голден',
+        subtitle: [
+            'Сорт - высший',
+            'Тип сушки - Теневая',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/osh-maiz.jpeg',
+        title: 'Изюм Малаяр',
+        subtitle: [
+            'Сорт - высший',
+            'Тип сушки - Теневая',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/chernosliv.jpg',
+        title: 'Чернослив',
+        subtitle: [
+            'Сорт - высший',
+            'Тип сушки - Теневая',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/kuraga.jpg',
+        title: 'Курага',
+        subtitle: [
+            'Сорт - высший',
+            'Тип сушки - Теневая',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/kuraga-organik.jpg',
+        title: 'Курага Органик',
+        subtitle: [
+            'Сорт - высший',
+            'Тип сушки - Теневая',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+
+    {
+        image: './Assets/images/dryfruits/kuraga-limonka.jpg',
+        title: 'Курага Лимонка',
+        subtitle: [
+            'Сорт - высший',
+            'Тип сушки - Теневая',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/injir.jpg',
+        title: 'Инжир Сушеный',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/dinya.jpeg',
+        title: 'Дыня Сушеная',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/kompot-smes.jpeg',
+        title: 'Компотная Смесь',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/mindal.jpg',
+        title: 'Миндаль',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/keshu.jpg',
+        title: 'Кешью',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/fistashki.jpeg',
+        title: 'Фисташки',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/orex-mestniy.jpg',
+        title: 'Орех Местный',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/gretski-oreh.jpeg',
+        title: 'Орех Грецкий',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/orex-skorlupe.jpg',
+        title: 'Орех В Скорлупе',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/arahis-v-skorlupe.jpg',
+        title: 'Арахис В Скорлупе',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/arahis-jarenit-soleniy.jpeg',
+        title: 'Арахис Жареный Соленый',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/arahis-kunjut-saxarom.jpg',
+        title: 'Арахис В Кунжуте С Сахаром',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/arahis-jareniy.jpg',
+        title: 'Арахис Жареный',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/mash.jpg',
+        title: 'Маш',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/fasol-krasnaya.jpg',
+        title: 'Фасоль Красная',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/fasol-belaya.jpg',
+        title: 'Фасоль Белая',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/kapersi.jpg',
+        title: 'Каперсы',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    {
+        image: './Assets/images/dryfruits/gorox.jpg',
+        title: 'Горох',
+        subtitle: [
+            'Сорт - высший',
+            'Упаковка - коробка 10кг'
+        ]
+    },
+    
+
+
+];
